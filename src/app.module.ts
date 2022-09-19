b8addfd4345e3bb41f2466a8b75e38a0fd74ac76
gowtamkumar <gowtampaul0@gmail.com>
2022-09-19 21:55:02 +0600

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '101',
+      database: 'bootcamp',
+      synchronize: true,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/config.schema.ts
@@ -0,0 +1,11 @@
+
+import Joi from "joi";
+
+export const configValidationSchema = Joi.object({
+  // API_PORT: Joi.number().default(3900).required(),
+  // DB_HOST: Joi.string().required(),
+  // DB_PORT: Joi.number().default(5432).required(),
+  // DB_USERNAME: Joi.string().required(),
+  // DB_PASSWORD: Joi.string().required(),
+  // DB_DATABASE: Joi.string().required(),
+})
